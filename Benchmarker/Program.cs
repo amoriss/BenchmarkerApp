f4715fd94336c474c821f21db43347fc5626000a
Amoriss <amoriss@protonmail.com>
2022-06-14 22:53:37 -0500

    moved demo class to its own file

--- Benchmarker/Program.cs
+++ Benchmarker/Program.cs
@@ -13,35 +13,4 @@ namespace Benchmarker
             var results = BenchmarkRunner.Run<Demo>();
         }
     }
-
-    [SimpleJob(RuntimeMoniker.Net472, baseline:true)]
-    [SimpleJob(RuntimeMoniker.Net50)]
-    [MemoryDiagnoser]
-    public class Demo
-    {
-        [Benchmark(Baseline = true)]
-        public string GetFullStringNormally()
-        {
-            string output = "";
-            for (int i = 0; i < 100; i++)
-            {
-                output += i;
-            }
-
-            return output;
-        }
-
-        [Benchmark]
-        public string GetFullStringWithStringBuilder()
-        {
-            StringBuilder output = new StringBuilder();
-
-            for (int i = 0; i < 100; i++)
-            {
-                output.Append(i);
-            }
-
-            return output.ToString();
-        }
-    }
 }
